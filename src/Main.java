6948fe0961f80c35db242538fa45d8c6f5e272e8
Evolazman <namzalove11000@gmail.com>
2025-06-24 02:49:31 +0700

    Day 1: Add user input greeting

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter username");
+        String userName = input.nextLine();
+
+        System.out.println("Hello Mr." + userName + " welcome to Java!");
     }
 }
